c65d929a72ba625cf3a172175049db0b1f13bc0d
edwtog <edwtog@yahoo.com>
2022-07-15 22:51:29 -0500

    kaggle lib test 01

--- kaggle.py
+++ kaggle.py
@@ -1,10 +1,14 @@
 import os
-os.environ['KAGGLE_USERNAME'] = "<your-kaggle-username>"
-os.environ['KAGGLE_KEY'] = "<your-kaggle-api-key>"
+#os.environ['KAGGLE_USERNAME'] = "<your-kaggle-username>"
+#os.environ['KAGGLE_KEY'] = "<your-kaggle-api-key>"
 
 from kaggle.api.kaggle_api_extended import KaggleApi
 
+#kaggle datasets download -d wkirgsn/electric-motor-temperature
+dataset_1 = "https://www.kaggle.com/datasets/wkirgsn/electric-motor-temperature"
 api = KaggleApi()
 api.authenticate()
+api.dataset_download_files("wkirgsn/electric-motor-temperature", "data/raw/electric-motor-temperature")
+
 
 print("Done!!")
