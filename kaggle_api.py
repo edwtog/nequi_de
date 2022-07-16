e9fccbb1cbe533d299f8999308f6a5598d98ac88
edwtog <edwtog@yahoo.com>
2022-07-15 23:26:37 -0500

    add new dataset

--- kaggle_api.py
+++ kaggle_api.py
@@ -5,10 +5,11 @@ import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 #kaggle datasets download -d wkirgsn/electric-motor-temperature
-dataset_1 = "https://www.kaggle.com/datasets/wkirgsn/electric-motor-temperature"
+dataset_1 = "wkirgsn/electric-motor-temperature"
 api = KaggleApi()
 api.authenticate()
-api.dataset_download_files("wkirgsn/electric-motor-temperature", "data/raw/electric-motor-temperature", unzip=True)
+api.dataset_download_files("wkirgsn/electric-motor-temperature", "data/raw/electric-motor-temperature/", unzip=True)
+api.dataset_download_files("ranadeep/credit-risk-dataset", "data/raw/credit-risk/", unzip=True)
 
 
 print("Done !!")
