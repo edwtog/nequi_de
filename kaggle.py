1e630d71e8ab637ed8795902663b1cc67eb8e635
edwtog <edwtog@yahoo.com>
2022-07-15 22:35:26 -0500

    Add kaggle credentials

--- kaggle.py
+++ kaggle.py
@@ -1,3 +1,10 @@
+import os
+os.environ['KAGGLE_USERNAME'] = "<your-kaggle-username>"
+os.environ['KAGGLE_KEY'] = "<your-kaggle-api-key>"
+
 from kaggle.api.kaggle_api_extended import KaggleApi
+
 api = KaggleApi()
 api.authenticate()
+
+print("Done!!")
