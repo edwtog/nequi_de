0bd9884b45b53ab5c27ffc5d2239ba9d587136a0
edwtog <edwtog@yahoo.com>
2022-07-16 11:42:51 -0500

    Add logging

--- kaggle_api.py
+++ kaggle_api.py
@@ -1,15 +1,25 @@
+import logging
 import os
+import infra
+import time
 #os.environ['KAGGLE_USERNAME'] = "<your-kaggle-username>"
 #os.environ['KAGGLE_KEY'] = "<your-kaggle-api-key>"
-
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-#kaggle datasets download -d wkirgsn/electric-motor-temperature
-dataset_1 = "wkirgsn/electric-motor-temperature"
-api = KaggleApi()
-api.authenticate()
-api.dataset_download_files("wkirgsn/electric-motor-temperature", "data/raw/electric-motor-temperature/", unzip=True)
-api.dataset_download_files("ranadeep/credit-risk-dataset", "data/raw/credit-risk/", unzip=True)
+def main():
+    file_date = time.strftime("%Y%m%d-%H%M%S")
+    logging.basicConfig(filename='logs/'+file_date+'.log', level=logging.INFO)
+    logging.info('Started infra creation')
+    infra.create_infra()
+    logging.info('Finished infra creation')
 
+    #kaggle datasets download -d wkirgsn/electric-motor-temperature
+    dataset_1 = "wkirgsn/electric-motor-temperature"
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files("wkirgsn/electric-motor-temperature", "data/raw/electric-motor-temperature/", unzip=True)
+    api.dataset_download_files("ranadeep/credit-risk-dataset", "data/raw/credit-risk/", unzip=True)
+    # kaggle datasets download -d harshithgupta/youtubes-channels-dataset
 
-print("Done !!")
+if __name__ == '__main__':
+    main()
