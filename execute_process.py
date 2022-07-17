e7986b0c3bd5b971c73b777cbd0b8458dcc75c9a
edwtog <edwtog@yahoo.com>
2022-07-17 17:15:06 -0500

    json transform

--- execute_process.py
+++ execute_process.py
@@ -1,10 +1,12 @@
 import logging
 import os
 import time
+import json
 import pandas as pd
 import src.infra as infra
 from src.load_data import load_datasets
 from src.load_datasets_S3 import upload_datasets
+from src.transform_json import trf_json
 
 from utils.definitions import ROOT_DIR
 
@@ -13,7 +15,7 @@ if __name__ == '__main__':
 
     file_date = time.strftime("%Y%m%d-%H%M%S")
     logging.basicConfig(filename='logs/'+file_date+'.log', level=logging.INFO)
-    
+    '''
     logging.info('Started infra creation')
     infra.create_infra()
     logging.info('Finished infra creation')
@@ -25,8 +27,13 @@ if __name__ == '__main__':
     logging.info('Uploading datasets to S3')
     if upload_datasets():
         logging.info('datasets loaded to S3')
+    '''
+    #rel_path = "data/raw/credit-risk/loan/loan.csv"
+    #abs_file_path = os.path.join(ROOT_DIR, rel_path)
+    #data = pd.read_csv(abs_file_path)
+    #print(data.shape)
 
-    rel_path = "data/raw/credit-risk/loan/loan.csv"
-    abs_file_path = os.path.join(ROOT_DIR, rel_path)
-    data = pd.read_csv(abs_file_path)
-    print(data.shape)
+    # json files
+    if trf_json():
+        logging.info('json to parquet')
+
