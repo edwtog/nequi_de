011158ea1289c310ba9783a26938efd631bf60e1
edwtog <edwtog@yahoo.com>
2022-07-18 11:27:02 -0500

    update unit test

--- test/test_definitions.py
+++ test/test_definitions.py
@@ -5,9 +5,9 @@ from utils import definitions
 class TestDefinitions(unittest.TestCase):
     def test_root(self):
         """
-        Test that it returns the correct path
+        Test that it returns the correct bucket name
         """
-        result = definitions.PRUEBA
+        result = definitions.BUCKET_NAME
         self.assertEqual(result, 20)
 
 if __name__ == '__main__':
